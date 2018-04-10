e0bef671ed99336d8a35c14a55aaa415485d9043
RafalDrgas <rafinho91@gmail.com>
2018-04-10 19:20:05 +0200

    String Calc TEST5

--- src/main/java/com/sda/stringCalc/StringCalculator.java
+++ src/main/java/com/sda/stringCalc/StringCalculator.java
@@ -2,17 +2,17 @@ package com.sda.stringCalc;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(String numbers){
         String textWithoutWhitespaces = StringUtils.deleteWhitespace(numbers);
         String[] elements = StringUtils.split(textWithoutWhitespaces,";");
         elements = elements == null ? new String [0] : elements;
-        int result = 0;
-        for (String element : elements) {
-            result += Integer.valueOf(element);
-        }
-        return result;
+        return Arrays.stream(elements)
+                .mapToInt(e -> Integer.valueOf(e))
+                .sum();
     }
 
 }
