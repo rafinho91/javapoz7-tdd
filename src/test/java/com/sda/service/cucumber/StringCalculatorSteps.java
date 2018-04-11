f1183dbbc48f48507d9763eec3f0a72377b1e4f0
RafalDrgas <rafinho91@gmail.com>
2018-04-11 19:01:07 +0200

    StringCalc UPDATE tests

--- src/test/java/com/sda/service/cucumber/StringCalculatorSteps.java
+++ src/test/java/com/sda/service/cucumber/StringCalculatorSteps.java
@@ -19,9 +19,19 @@ public class StringCalculatorSteps {
         this.calculator = new StringCalculator();
     }
 
-    @Given("^I pass single number value$")
-    public void I_pass_single_number_value(){
-        this.value = "5";
+    @Given("^I pass (.*) value$")
+    public void I_pass_single_number_value(String text){
+        this.value = text;
+    }
+
+    @And("^I pass value null$")
+    public void i_Pass_Null_Value() {
+        this.value = null;
+    }
+
+    @Then("^I get (.*) as a result$")
+    public void I_get_5_as_a_result(int passedResult){
+        Assert.assertEquals(passedResult, result);
     }
 
     @When("^I trigger calculate function$")
@@ -29,48 +39,4 @@ public class StringCalculatorSteps {
         this.result = calculator.add(value);
     }
 
-    @Then("^I get 5 as a result$")
-    public void I_get_5_as_a_result(){
-        Assert.assertEquals(5, result);
-    }
-
-    @And("^I pass null value$")
-    public void i_Pass_Null_Value() {
-        this.value = null;
-    }
-
-    @Then("^I get 0 as a result$")
-    public void i_Get_0_As_A_Result(){
-        Assert.assertEquals(0, result);
-    }
-
-    @And("^I pass blank value$")
-    public void i_Pass_Blank_Value(){
-        this.value = "";
-    }
-
-    @And("^I pass numbers 3 and 5$")
-    public void i_Pass_Numbers_3_And_5() {
-        this.value = "3;5";
-    }
-
-    @Then("^I get 8 as a result$")
-    public void i_Get_8_As_A_Result() {
-        Assert.assertEquals(8, result);
-    }
-
-    @And("^I pass numbers 1 2 3 and 4$")
-    public void i_Pass_Numbers_1_2_3_And_4(){
-        this.value = "1;2;3;4";
-    }
-
-    @Then("^I get 10 as a result$")
-    public void i_Get_10_As_A_Result() {
-        Assert.assertEquals(10, result);
-    }
-
-    @And("^I pass 1 2 3 4 with whitespaces$")
-    public void i_Pass_With_Whitespaces() {
-        this.value = "1; 2; 3; 4";
-    }
 }
--- src/test/resources/com/sda/service/cucumber/StringCalculator.feature
+++ src/test/resources/com/sda/service/cucumber/StringCalculator.feature
@@ -1,36 +1,36 @@
 Feature:
   Scenario: I can calculate single number
     Given I initialize stringCalculator
-    And I pass single number value
+    And I pass 5 value
     When I trigger calculate function
     Then I get 5 as a result
 
   Scenario: I can calculate null number
     Given I initialize stringCalculator
-    And I pass null value
+    And I pass value null
     When I trigger calculate function
     Then I get 0 as a result
 
   Scenario: I can calculate blank value
     Given I initialize stringCalculator
-    And I pass blank value
+    And I pass  value
     When I trigger calculate function
     Then I get 0 as a result
 
   Scenario: I can calculate two numbers
     Given I initialize stringCalculator
-    And I pass numbers 3 and 5
+    And I pass 3;5 value
     When I trigger calculate function
     Then I get 8 as a result
 
   Scenario: I can calculate multiple numbers
     Given I initialize stringCalculator
-    And I pass numbers 1 2 3 and 4
+    And I pass 1;2;3;4 value
     When I trigger calculate function
     Then I get 10 as a result
 
   Scenario: I can calculate multiple with whitespaces
     Given I initialize stringCalculator
-    And I pass 1 2 3 4 with whitespaces
+    And I pass 1; 2; 3; 4 value
     When I trigger calculate function
     Then I get 10 as a result
